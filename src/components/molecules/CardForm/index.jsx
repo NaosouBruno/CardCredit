f8230fe12820b252fd01e78dba5c116bf22679d6
Bruno <brunoowap@gmail.com>
2022-09-08 14:07:41 -0300

    criando input de numero doc artao

--- src/components/molecules/CardForm/index.jsx
+++ src/components/molecules/CardForm/index.jsx
@@ -13,7 +13,18 @@ function CardForm() {
           type="text"
           id="cardName"
           className="form__input"
-          placeholder="e.g Jnae Appleseed"
+          placeholder="e.g. Jnae Appleseed"
+        />
+      </div>
+      <div className="form__container">
+        <label htmlFor="cardNumber" className="form__description">
+          Card Number
+        </label>
+        <input
+          type="text"
+          id="cardNumber"
+          className="form__input"
+          placeholder="e.g. 1234 5678 9123 0000"
         />
       </div>
       <button className="form__btnSubmit">Confirm</button>
