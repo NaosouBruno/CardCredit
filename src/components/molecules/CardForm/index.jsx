5b528a419ed408e698b478825705d91f5bd20587
Bruno <brunoowap@gmail.com>
2022-09-08 15:17:58 -0300

    iniciando inputs de data

--- src/components/molecules/CardForm/index.jsx
+++ src/components/molecules/CardForm/index.jsx
@@ -16,6 +16,7 @@ function CardForm() {
           placeholder="e.g. Jnae Appleseed"
         />
       </div>
+
       <div className="form__container">
         <label htmlFor="cardNumber" className="form__description">
           Card Number
@@ -26,6 +27,40 @@ function CardForm() {
           className="form__input"
           placeholder="e.g. 1234 5678 9123 0000"
         />
+      </div>
+
+      <div className="form__containerDate">
+        <div className="form__dateGroup">
+          <label htmlFor="cardDate" className="form__description">
+            Exp. Date (MM/YY)
+          </label>
+          <div className="form__dateGroup form__dateGroup--dateInput">
+            <input
+              type="number"
+              id="cardDate"
+              className="form__input form__input--date"
+              placeholder="MM"
+            />
+            <input
+              type="number"
+              id="cardDate"
+              className="form__input form__input--date"
+              placeholder="YY"
+            />
+          </div>
+        </div>
+
+        <div className="form__dateGroup">
+          <label htmlFor="cardCvc" className="form__description">
+            CVC
+          </label>
+          <input
+            type="number"
+            id="cardCvc"
+            className="form__input form__input--date"
+            placeholder="e.g 123"
+          />
+        </div>
       </div>
       <button className="form__btnSubmit">Confirm</button>
     </form>
