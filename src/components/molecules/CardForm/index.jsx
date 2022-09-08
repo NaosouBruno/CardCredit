18d44ce1f8955b04e51f495b291ba7a90f8ce26b
Bruno <brunoowap@gmail.com>
2022-09-08 14:04:28 -0300

    Criando inputs e estilizando

--- src/components/molecules/CardForm/index.jsx
+++ src/components/molecules/CardForm/index.jsx
@@ -1,6 +1,24 @@
 import "./form.scss";
 function CardForm() {
-  return <form className="form"></form>;
+  const submitForm = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <form onSubmit={submitForm} className="form">
+      <div className="form__container">
+        <label htmlFor="cardName" className="form__description">
+          Cardholder Name
+        </label>
+        <input
+          type="text"
+          id="cardName"
+          className="form__input"
+          placeholder="e.g Jnae Appleseed"
+        />
+      </div>
+      <button className="form__btnSubmit">Confirm</button>
+    </form>
+  );
 }
 
 export default CardForm;
