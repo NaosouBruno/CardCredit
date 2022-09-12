e3462bfd89fce1486550c3c1206b34d7c308e11c
Bruno <brunoowap@gmail.com>
2022-09-12 12:18:08 -0300

    iniciando validação:

--- src/components/molecules/CardForm/index.jsx
+++ src/components/molecules/CardForm/index.jsx
@@ -1,8 +1,44 @@
+import { useState } from "react";
 import "./form.scss";
+const initialState = {
+  name: "",
+  /* number: "",
+  mm: "",
+  yy: "",
+  cvc: "", */
+};
+
 function CardForm() {
+  const [card, setCard] = useState(initialState);
+  const [nameHasError, setNameHasError] = useState(false);
+
   const submitForm = (event) => {
     event.preventDefault();
+    if (validForm()) {
+      alert("Deu certo");
+      setCard({ ...initialState });
+    }
+    console.log(card);
   };
+
+  const handlerChange = (event) => {
+    const value = event.target.value;
+    setCard({
+      ...card,
+      [event.target.name]: value,
+    });
+  };
+
+  const validForm = () => {
+    if (card.name.trim().length > 0) {
+      setNameHasError(false);
+      return true;
+    } else {
+      setNameHasError(true);
+      return false;
+    }
+  };
+
   return (
     <form onSubmit={submitForm} className="form">
       <div className="form__container">
@@ -12,8 +48,12 @@ function CardForm() {
         <input
           type="text"
           id="cardName"
-          className="form__input"
+          /* className="form__input" */
+          className={nameHasError ? "form__input error" : "form__input "}
           placeholder="e.g. Jnae Appleseed"
+          name="name"
+          value={card.name}
+          onChange={handlerChange}
         />
       </div>
 
@@ -22,7 +62,7 @@ function CardForm() {
           Card Number
         </label>
         <input
-          type="text"
+          type="number"
           id="cardNumber"
           className="form__input"
           placeholder="e.g. 1234 5678 9123 0000"
